3b879520a68669dff4cad34069feb4b1cf86d6f2
bilitixi <bilitixi@gmail.com>
2025-08-13 11:37:41 +1200

    ReportReview

---
+++ Kaioordinate/competitorReport.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Kaioordinate
+{
+    partial class competitorReport
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnReturn = new FontAwesome.Sharp.IconButton();
+            this.btnReport = new FontAwesome.Sharp.IconButton();
+            this.SuspendLayout();
+            // 
+            // btnReturn
+            // 
+            this.btnReturn.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReturn.IconChar = FontAwesome.Sharp.IconChar.SignOut;
+            this.btnReturn.IconColor = System.Drawing.Color.Black;
+            this.btnReturn.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnReturn.Location = new System.Drawing.Point(325, 101);
+            this.btnReturn.Name = "btnReturn";
+            this.btnReturn.Size = new System.Drawing.Size(132, 115);
+            this.btnReturn.TabIndex = 84;
+            this.btnReturn.Text = "RETURN";
+            this.btnReturn.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnReturn.UseVisualStyleBackColor = true;
+            // 
+            // btnReport
+            // 
+            this.btnReport.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReport.IconChar = FontAwesome.Sharp.IconChar.Print;
+            this.btnReport.IconColor = System.Drawing.Color.Black;
+            this.btnReport.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnReport.Location = new System.Drawing.Point(90, 101);
+            this.btnReport.Name = "btnReport";
+            this.btnReport.Size = new System.Drawing.Size(132, 115);
+            this.btnReport.TabIndex = 85;
+            this.btnReport.Text = "RETURN";
+            this.btnReport.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnReport.UseVisualStyleBackColor = true;
+            // 
+            // competitorReport
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(6)))), ((int)(((byte)(73)))), ((int)(((byte)(41)))));
+            this.ClientSize = new System.Drawing.Size(540, 242);
+            this.Controls.Add(this.btnReport);
+            this.Controls.Add(this.btnReturn);
+            this.Name = "competitorReport";
+            this.Text = "Report Review";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private FontAwesome.Sharp.IconButton btnReturn;
+        private FontAwesome.Sharp.IconButton btnReport;
+    }
+}
